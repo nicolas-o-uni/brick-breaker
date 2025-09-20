e6e5c4d7fbe8babd9f3a6555cf385c057c8c9745
Maíza Oliveira <maizamoreira@souunisuam.com.br>
2025-09-20 16:17:16 -0300

    Adiciona layout da página principal e ajustes das  rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
 
+
 export const routes: Routes = [
+  {
+    path: 'principal',
+    loadComponent: () => import('./pages/principal/principal.page').then(m => m.PrincipalPage)
+  },
+  {
+    path: '',
+    redirectTo: 'principal',
+    pathMatch: 'full',
+  },
   {
     path: 'map1',
     loadComponent: () => import('./pages/map1/map1.page').then((m) => m.GamePage),
   },
   {
-    path: '',
-    redirectTo: 'map1',
-    pathMatch: 'full',
-  },
-  {
     path: 'map2',
-    loadComponent: () => import('./pages/map2/map2.page').then( m => m.GamePage)
+    loadComponent: () => import('./pages/map2/map2.page').then(m => m.GamePage)
   },
 ];
+
+
